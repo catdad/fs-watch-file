688e4c9574bc968d0cc049606a7c090178d9bc3d
Kiril Vatev <vatev.1@gmail.com>
2019-05-02 22:04:49 -0400

    adding extra tests for options

--- test/index.test.js
+++ test/index.test.js
@@ -3,53 +3,72 @@ const path = require('path');
 const expect = require('chai').expect;
 const touch = require('touch');
 
-const lib = require('../')();
+const lib = require('../');
 const root = path.resolve(__dirname, '..');
 const fixture = name => path.resolve(root, 'fixtures', name);
 
 describe('module', () => {
-  beforeEach(() => {
-    lib.close();
-    lib.removeAllListeners();
-  });
-  afterEach(() => {
-    lib.close();
-    lib.removeAllListeners();
-  });
+  let instance;
 
-  it('listens to changes on a specific file', done => {
-    lib.add(fixture('one'));
-    lib.on('change', ev => {
-      expect(ev).to.deep.equal({ file: fixture('one') });
-      done();
+  function reset() {
+    if (instance) {
+      instance.close();
+      instance.removeAllListeners();
+    }
+  }
+
+  beforeEach(reset);
+  afterEach(reset);
+
+  [{
+    name: 'without options',
+    beforeEach: () => { instance = lib(); }
+  }, {
+    name: 'with { persistent: true }',
+    beforeEach: () => { instance = lib({ persistent: true }); }
+  }, {
+    name: 'with { persistent: false }',
+    beforeEach: () => { instance = lib({ persistent: false }); }
+  }].forEach(group => {
+    describe(group.name, () => {
+      beforeEach(group.beforeEach);
+
+      it('listens to changes on a specific file', done => {
+        instance.add(fixture('one'));
+        instance.on('change', ev => {
+          expect(ev).to.deep.equal({ file: fixture('one') });
+          done();
+        });
+
+        touch(fixture('one'));
+      });
+
+      it('can listen to changes on multiple files', done => {
+        const files = new Set();
+
+        instance.add(fixture('one'));
+        instance.add(fixture('two'));
+        instance.add(fixture('three'));
+
+        instance.on('change', ev => {
+          files.add(ev.file);
+
+          if (files.size === 3) {
+            expect(Array.from(files).sort()).to.deep.equal([
+              fixture('one'),
+              fixture('three'),
+              fixture('two'),
+            ]);
+
+            done();
+          }
+        });
+
+        touch(fixture('one'));
+        touch(fixture('two'));
+        touch(fixture('three'));
+      });
     });
-
-    touch(fixture('one'));
   });
 
-  it('can listen to changes on multiple files', done => {
-    const files = new Set();
-
-    lib.add(fixture('one'));
-    lib.add(fixture('two'));
-    lib.add(fixture('three'));
-
-    lib.on('change', ev => {
-      files.add(ev.file);
-
-      if (files.size === 3) {
-        expect(Array.from(files).sort()).to.deep.equal([
-          fixture('one'),
-          fixture('three'),
-          fixture('two'),
-        ]);
-
-        done();
-      }
-    });
-
-    touch(fixture('one'));
-    touch(fixture('two'));
-    touch(fixture('three'));
-  });
 });
